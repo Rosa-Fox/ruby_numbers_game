4f02874435410ea2b41579d9a75bd5ed0b76731d
Rosa-Fox <rosafox89@gmail.com>
2015-05-05 12:41:37 +0100

    Game is repeated 10 times before it ends

--- numbers_2.rb
+++ numbers_2.rb
@@ -1,13 +1,17 @@
 #!/usr/bin/env ruby
-x = Random.rand(5..10)
-y = Random.rand(5..10)
 
-print "#{x} + #{y} = "
-answer = gets.to_i
+turns = 0
+while turns < 10
+  x = Random.rand(5..10)
+  y = Random.rand(5..10)
+  turns = turns + 1
+  print "#{x} + #{y} = "
+  answer = gets.to_i
 
-if answer == x + y
-       puts "Right"
-else 
- puts "Wrong"
+  if answer == x + y
+    puts "Right"
+  else
+    puts "Wrong"
+  end
 end
 
