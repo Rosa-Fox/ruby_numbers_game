967e7f0e22894e17db0eed537dcf0b2e43a4bfb9
Rosa-Fox <rosafox89@gmail.com>
2015-05-05 12:52:17 +0100

    Can set the number of attempts and prints out the number right, wrong and the overall percentage that were correct

--- numbers_2.rb
+++ numbers_2.rb
@@ -1,7 +1,10 @@
 #!/usr/bin/env ruby
 
 turns = 0
-while turns < 10
+correct = 0
+wrong = 0
+attempts = 2
+while turns < attempts
   x = Random.rand(5..10)
   y = Random.rand(5..10)
   turns = turns + 1
@@ -10,8 +13,12 @@ while turns < 10
 
   if answer == x + y
     puts "Right"
+    correct = correct + 1
   else
     puts "Wrong"
+    wrong = wrong + 1
   end
-end
 
+  score = 100 * correct / attempts
+end
+puts "Rights #{correct}; Wrongs #{wrong}, Score #{score}%"
